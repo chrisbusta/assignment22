45e58806418020548f48d57589573a9803472cfc
chris bustamante <chrisbusta@hotmsil.com>
2015-07-17 09:48:29 -0400

    entered new angular form

--- app/js/pages/dashboard/dashboard.controller.js
+++ app/js/pages/dashboard/dashboard.controller.js
@@ -36,74 +36,17 @@
 
 
 
-// }());
-// (function(){
-//   'use strict';
-
-var Todo = function(options){
-
-  var args = options || {};
-  this.task = args.task;
-  this.status = 'open';
-
-};
-
-var bin = [];
-
-$('#todoform').on('submit', function(e){
-  e.preventDefault();
-
-  var taskText = $("#new-text").val();
-  var taskInstance = new Todo({task: taskText});
-
-  bin.push(taskInstance);
-  $("#todo").append('<li>' + taskText + '</li>');
-  this.reset();
-});
-
-$('#todo').on('click', 'li', function(e){
-  e.preventDefault();
-  $(this).addClass("complete");
-
- var tTask = $(this).text();
-
- var textEdit = _.find(bin, {task: tTask});
- textEdit.status = 'closed';
-
-});
-
-$('#todo').on('click', '.complete', function(e){
-  e.preventDefault();
-  $(this).removeClass("complete");
-
- var tTask = $(this).text();
-
- var textEdit = _.find(bin, {task: tTask});
- textEdit.status = 'open';
-
-});
-
-$('#remove-button').on('click', function(){
-  bin = bin.filter(function(l){
-   if(l.status === 'open'){
-    return l;
-   }
-   bin.push(l);
-
-
-  });
-  $('#todo').empty();
-  bin.forEach(function(l){
-  $("#todo").append('<li>' + l.task + '</li>');
-  });
-
-});
-
-
-
-
-
-
-
 }());
 
+
+angular.module('submitExample', [])
+    .controller('ExampleController', ['$scope', function($scope) {
+      $scope.list = [];
+      $scope.text = 'hello';
+      $scope.submit = function() {
+        if ($scope.text) {
+          $scope.list.push(this.text);
+          $scope.text = '';
+        }
+      };
+    }]);
