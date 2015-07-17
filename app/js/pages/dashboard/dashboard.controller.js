1afce09c5f486b2087db6d37bb6156ad68ff41fc
chris bustamante <chrisbusta@hotmsil.com>
2015-07-17 13:13:56 -0400

    changes

--- app/js/pages/dashboard/dashboard.controller.js
+++ app/js/pages/dashboard/dashboard.controller.js
@@ -36,17 +36,40 @@
 
 
 
+function TodoCtrl($scope) {
+
+  $scope.todos = [
+    {text:'Learn AngularJS', done:false},
+    {text: 'Build an app', done:false}
+  ];
+
+  $scope.getTotalTodos = function () {
+    return $scope.todos.length;
+  };
+
+
+  $scope.addTodo = function () {
+    $scope.todos.push({text:$scope.formTodoText, done:false});
+    $scope.formTodoText = '';
+  };
+
+    $scope.clearCompleted = function () {
+        $scope.todos = _.filter($scope.todos, function(todo){
+            return !todo.done;
+        });
+    };
+}
 }());
 
 
-angular.module('submitExample', [])
-    .controller('ExampleController', ['$scope', function($scope) {
-      $scope.list = [];
-      $scope.text = 'hello';
-      $scope.submit = function() {
-        if ($scope.text) {
-          $scope.list.push(this.text);
-          $scope.text = '';
-        }
-      };
-    }]);
+// angular.module('submitExample', [])
+//     .controller('ExampleController', ['$scope', function($scope) {
+//       $scope.list = [];
+//       $scope.text = 'hello';
+//       $scope.submit = function() {
+//         if ($scope.text) {
+//           $scope.list.push(this.text);
+//           $scope.text = '';
+//         }
+//       };
+//     }]);
